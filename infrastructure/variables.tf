879c208551e4665e483268e086a09968eedd5236
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2025-02-09 17:24:13 +0100

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -44,3 +44,18 @@ variable "service_account" {
   type        = string
   default     = "ci-service-account"
 }
+
+variable "github_token" {
+  description = "The GitHub token to use for authenticating with the GitHub provider."
+  type        = string
+}
+
+variable "github_owner" {
+  description = "The owner of the GitHub repository to use for the GitHub provider."
+  type        = string
+}
+
+variable "github_repository" {
+  description = "The name of the GitHub repository to use for the GitHub provider."
+  type        = string
+}
