20a472d4cfb1d936ab5ccd09773b13f3c013f8ff
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2025-02-11 14:10:48 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -172,8 +172,8 @@ resource "google_service_account_key" "ci_service_account_key" {
 }
 
 resource "github_actions_environment_secret" "ci_service_account_json" {
-  repository      = var.github_repository   # ex: "mc-all-item-adventure"
-  environment     = "prod"                  # l'environnement ciblé
+  repository      = var.github_repository # ex: "mc-all-item-adventure"
+  environment     = "prod"                # l'environnement ciblé
   secret_name     = "SERVICE_ACCOUNT_KEY"
   plaintext_value = base64decode(google_service_account_key.ci_service_account_key.private_key)
 }
@@ -214,8 +214,8 @@ resource "google_service_account_key" "cloud_run_plugin_firebase_api_service_acc
 }
 
 resource "github_actions_environment_secret" "cloud_run_plugin_firebase_api_service_account_json" {
-  repository      = var.github_repository   # ex: "mc-all-item-adventure"
-  environment     = "prod"                  # l'environnement ciblé
+  repository      = var.github_repository # ex: "mc-all-item-adventure"
+  environment     = "prod"                # l'environnement ciblé
   secret_name     = "CLOUD_RUN_EXECUTOR_SERVICE_ACCOUNT"
   plaintext_value = base64decode(google_service_account_key.cloud_run_plugin_firebase_api_service_account_key.private_key)
 }
