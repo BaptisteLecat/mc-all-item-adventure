a138473f9756b18d58eca8c4664dab5716271c39
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2025-02-11 14:56:11 +0100

    Fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -219,7 +219,7 @@ resource "github_actions_environment_secret" "cloud_run_plugin_firebase_api_serv
   repository      = var.github_repository # ex: "mc-all-item-adventure"
   environment     = "prod"                # l'environnement ciblé
   secret_name     = "CLOUD_RUN_EXECUTOR_SERVICE_ACCOUNT_NAME"
-  plaintext_value = base64decode(google_service_account.cloud_run_plugin_firebase_api_service_account.email)
+  plaintext_value = google_service_account.cloud_run_plugin_firebase_api_service_account.email
 }
 
 # Enable the Cloud Billing API
